84582f3070189b19906c236870dc9e884e11ad06
Colin Leach <colin.leach@comcast.net>
2022-04-16 15:34:33 -0700

    more on predictions

--- projcode/predictions/view_predictions.py
+++ projcode/predictions/view_predictions.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import numpy as np
 import pandas as pd
-import os
 import h5py
 
 from projcode.data.utils import read_params
@@ -36,25 +35,73 @@ class ViewPredictions:
     def write_feather(self, feather_file: Path):
         self.results.to_feather(feather_file)
 
-    def make_summaries(self):
-        summaries = self.ids.copy()
+    @staticmethod
+    def summarize(df: pd.DataFrame, output: pd.DataFrame):
+        """
+
+        :param df:
+        :param output:
+        :return:
+        """
         for question in decals_pairs:
             options = decals_pairs[question]
             pairs = [question + opt for opt in options]
-            result_subset = self.results.loc[:, pairs]
+            df_subset = df.loc[:, pairs]
             summary = pd.DataFrame()
-            summary['idxmax'] = ((result_subset.idxmax(axis=1)).str.split('_'))
-            summary['maxprob'] = result_subset.max(axis=1, numeric_only=True) \
-                                 / result_subset.sum(axis=1, numeric_only=True)
+            summary['idxmax'] = ((df_subset.idxmax(axis=1)).str.split('_'))
+            summary['maxprob'] = df_subset.max(axis=1, numeric_only=True) \
+                                 / df_subset.sum(axis=1, numeric_only=True)
             summary['choice'] = summary.values.tolist()
-            summaries[question] = summary['choice']
-        self.summaries = summaries
+            output[question] = summary['choice']
+        return output
+
+    def make_prediction_summaries(self):
+        """
+
+        :return:
+        """
+        summaries = self.ids.copy()
+        self.summaries = self.summarize(self.results, summaries)
+
+    def make_observed_summaries(self, catalog_file: Path) -> None:
+        """
+
+        :param catalog_file:
+        :return:
+        """
+        self.obs = pd.read_csv(catalog_file)
+        summaries = pd.DataFrame(self.obs['file_loc'], columns=['file_loc', ])
+        self.obs_summaries = self.summarize(self.obs, summaries)
+
+    def one_comparison(self, inx: int) -> pd.DataFrame:
+        """
+
+        :param inx:
+        :return:
+        """
+
+        example = self.summaries.iloc[inx]
+        questions = self.summaries.columns.values[1:]
+        preds = [f"{q}: {example[q][0][1]}, ({example[q][1]:.2f})" for q in questions]
+        obs = self.obs_summaries.loc[self.obs['file_loc'] == example['file_loc']].iloc[0]
+
+        comp = pd.DataFrame(columns=['question', 'pred', 'pred_confidence', 'obs', 'obs_confidence'])
+        comp['question'] = questions
+        comp['pred'] = [example[q][0][1] for q in questions]
+        comp['pred_confidence'] = [f"{example[q][1]:.2f}" for q in questions]
+        comp['obs'] = [obs[q][0][1] for q in questions]
+        comp['obs_confidence'] = [obs[q][1] for q in questions]
+        return example['file_loc'], comp
 
 
 if __name__ == "__main__":
     params = read_params()
     dataroot = Path(params['dataroot'])
     save_loc = dataroot / 'results/predictions/decals.hdf5'
+    catalog_file = dataroot / 'shards/decals/test_shards/test_df.csv'
     vp = ViewPredictions()
     vp.read_predictions(save_loc)
-    vp.make_summaries()
+    vp.make_prediction_summaries()
+    vp.make_observed_summaries(catalog_file)
+    comp = vp.one_comparison(20)
+    print(comp)
